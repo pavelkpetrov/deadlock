e78e9f715cc2493b611212460d53e57864993f67
pavelkpetrov <p.petrov@softteco.com>
2019-10-15 23:43:59 +0300

    add initial commit

---
+++ src/main/java/com/softteco/Main.java
@@ -0,0 +1,68 @@
+package com.softteco;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        test1();
+        detectDeadlock();
+
+    }
+
+    private static void test1() {
+        final Object lock1 = new Object();
+        final Object lock2 = new Object();
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override public void run() {
+                synchronized (lock1) {
+                    System.out.println("Thread "+ Thread.currentThread().getId() +" acquired lock1");
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(50);
+                    } catch (InterruptedException ignore) {}
+                    synchronized (lock2) {
+                        System.out.println("Thread "+ Thread.currentThread().getId() +" acquired lock2");
+                    }
+                }
+            }
+
+        });
+        thread1.start();
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override public void run() {
+                synchronized (lock2) {
+                    System.out.println("Thread "+ Thread.currentThread().getId() +" acquired lock2");
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(50);
+                    } catch (InterruptedException ignore) {}
+                    synchronized (lock1) {
+                        System.out.println("Thread "+ Thread.currentThread().getId() +" acquired lock1");
+                    }
+                }
+            }
+        });
+        thread2.start();
+
+        // Wait a little for threads to deadlock.
+        try {
+            TimeUnit.MILLISECONDS.sleep(100);
+        } catch (InterruptedException ignore) {}
+    }
+
+    private static void detectDeadlock() {
+        ThreadMXBean threadBean = ManagementFactory.getThreadMXBean();
+        long[] threadIds = threadBean.findMonitorDeadlockedThreads();
+        int deadlockedThreads = threadIds != null? threadIds.length : 0;
+        System.out.println("Number of deadlocked threads: " + deadlockedThreads);
+        for (long threadId : threadIds) {
+            System.out.println("Deadlocked thread id : " + threadId);
+        }
+    }
+
+}
